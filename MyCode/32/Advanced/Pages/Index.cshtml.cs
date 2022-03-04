ce260717a09bd279659a35e3eb066f3fedb081c6
Wagner Xu <wagnerhsu@hotmail.com>
2022-03-04 11:25:08 +0800

    add files

---
+++ MyCode/32/Advanced/Pages/Index.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Advanced.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ MyCode/32/Advanced/Program.cs
@@ -0,0 +1,30 @@
+﻿using Advanced.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+
+builder.Services.AddDbContext<DataContext>(opts =>
+{
+    opts.UseSqlServer(builder.Configuration["ConnectionStrings:PeopleConnection"]);
+    opts.EnableSensitiveDataLogging(true);
+});
+
+var app = builder.Build();
+
+//app.MapGet("/", () => "Hello World!");
+
+app.UseStaticFiles();
+
+app.MapControllers();
+app.MapControllerRoute("controllers",
+    "controllers/{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
+
+var context = app.Services.CreateScope().ServiceProvider
+    .GetRequiredService<DataContext>();
+SeedData.SeedDatabase(context);
+
+app.Run();
