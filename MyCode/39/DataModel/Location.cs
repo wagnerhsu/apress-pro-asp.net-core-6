2300ed4cd23c68e21ac59a89fd4aae6322f3ab2a
Wagner.Xu <wagnerhsu@hotmail.com>
2022-04-05 22:47:21 +0800

    add files

---
+++ MyCode/39/DataModel/Location.cs
@@ -0,0 +1,13 @@
+﻿using Advanced.Models;
+
+namespace DataModel
+{
+    public class Location
+    {
+        public long LocationId { get; set; }
+        public string City { get; set; } = string.Empty;
+        public string State { get; set; } = string.Empty;
+
+        public IEnumerable<Person>? People { get; set; }
+    }
+}
