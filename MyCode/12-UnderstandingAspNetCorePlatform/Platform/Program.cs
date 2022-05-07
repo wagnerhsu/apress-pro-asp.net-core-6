b6194c097935d286a4d4704310569f36d0d2e2dd
Wagner.Xu <wagnerhsu@hotmail.com>
2022-05-07 14:20:31 +0800

    12-UnderstandingAspNetCorePlatform

---
+++ MyCode/12-UnderstandingAspNetCorePlatform/Platform/Program.cs
@@ -0,0 +1,26 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.Configure<MessageOptions>(options =>
+{
+    options.CityName = "Albany";
+});
+
+var app = builder.Build();
+IApplicationBuilder appBuilder = app;
+appBuilder.Use(async (httpContext, next) =>
+{
+    await httpContext.Response.WriteAsync("Hi， \n");
+    await next(httpContext);
+});
+
+app.Use(async (context, next) =>
+{
+    context.Response.ContentType = "text/html";
+    await context.Response.WriteAsync("Middleware01\n");
+    await next();
+});
+app.UseMiddleware<LocationMiddleware>();
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
