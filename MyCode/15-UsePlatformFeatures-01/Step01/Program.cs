c61a9ce41284a06a9bba34df6542d0a9f2f77e27
Wagner Xu <wagnerhsu@hotmail.com>
2022-08-05 21:05:07 +0800

    update files

---
+++ MyCode/15-UsePlatformFeatures-01/Step01/Program.cs
@@ -0,0 +1,22 @@
+﻿using Serilog;
+using Serilog.WxLibrary;
+
+var serilogService = new SerilogService(SerilogService.DefaultOptions);
+serilogService.Initialize();
+var builder = WebApplication.CreateBuilder(args);
+builder.Host.ConfigureLogging(logging =>
+{
+    logging.ClearProviders();
+    logging.SetMinimumLevel(LogLevel.Trace);
+    logging.AddSerilog();
+});
+var app = builder.Build();
+
+app.Map("/", async context =>
+{
+    var loggerFactory = context.RequestServices.GetService<ILoggerFactory>();
+    var logger = loggerFactory.CreateLogger<Program>();
+    logger.LogInformation("Hello World");
+    await context.Response.WriteAsync("Hello World!");
+});
+app.Run();
---
+++ MyCode/15-UsePlatformFeatures-01/Step01/README.md
@@ -0,0 +1,5 @@
+﻿# README
+
+## 2022-08-05
+
+* Create this file
